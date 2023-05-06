848d3196e2aa965b06246cd1d6da06a72603ef07
siwookim97 <tldn12gh@gmail.com>
2023-05-07 04:22:40 +0900

    init setting

---
+++ src/main/java/com/ll/kotudy/KotudyRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.ll.kotudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotudyRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotudyRefactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/kotudy/KotudyRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.kotudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotudyRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
