382bbfa818039be1090e5142cbfdadcbdbc2a893
siwookim97 <tldn12gh@gmail.com>
2023-05-12 17:19:56 +0900

    test: name과 morpheme필드를 사용해 삭제하는 쿼리 테스트 코드 추가

--- src/test/java/com/ll/kotudy/word/domain/MyWordRepositoryTest.java
+++ src/test/java/com/ll/kotudy/word/domain/MyWordRepositoryTest.java
@@ -2,6 +2,7 @@ package com.ll.kotudy.word.domain;
 
 import com.ll.kotudy.word.service.dto.QuizWordDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,9 +28,11 @@ class MyWordRepositoryTest {
             myWordRepository.save(new MyWord("동물" + i, "명사",
                     "사람을 제외한 길짐승, 날짐승, 물짐승 따위를 통틀어 이르는 말." + i));
         }
+        myWordRepository.flush();
     }
 
     @Test
+    @DisplayName("name과 morpheme필드를 사용해 db에 있는지 확인 쿼리 테스트")
     void existsByNameAndMorphemeTest() {
 
         boolean exist = myWordRepository.existsByNameAndMorpheme("동물1", "명사");
@@ -40,7 +43,8 @@ class MyWordRepositoryTest {
     }
 
     @Test
-    void findMyWordRandomForQuiz_Querydsl() {
+    @DisplayName("퀴즈를 위한 단어 가져오는 쿼리 테스트")
+    void findMyWordDistnctRandomForQuizTest() {
 
         List<QuizWordDto> myWordDistnctRandomForQuiz = myWordRepository.findMyWordDistnctRandomForQuiz();
 
@@ -48,5 +52,19 @@ class MyWordRepositoryTest {
         for (QuizWordDto quizWordDto : myWordDistnctRandomForQuiz) {
             assertThat(quizWordDto).isInstanceOf(QuizWordDto.class);
         }
+    }
+
+    @Test
+    @DisplayName("name과 morpheme필드를 사용해 삭제하는 쿼리 테스트")
+    void deleteByNameAndMorphemeTest() {
+
+        myWordRepository.deleteByNameAndMorpheme("동물20", "명사");
+        myWordRepository.deleteByNameAndMorpheme("동물21", "명사");
+        List<MyWord> findMyWord = myWordRepository.findAll();
+
+        assertThat(findMyWord.size()).isEqualTo(9999);
+        assertThat(myWordRepository.existsByNameAndMorpheme("동물20", "명사")).isFalse();
+        assertThat(myWordRepository.existsByNameAndMorpheme("동물21", "명사")).isFalse();
+        assertThat(myWordRepository.existsByNameAndMorpheme("동물22", "명사")).isTrue();
     }
 }
