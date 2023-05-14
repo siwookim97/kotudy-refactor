476a34c6d24f10d280698715c78e1ccc44784ec0
siwookim97 <tldn12gh@gmail.com>
2023-05-14 18:08:13 +0900

    test: 회원가입 테스트 추가

---
+++ src/test/java/com/ll/kotudy/member/controller/MemerControllerTest.java
@@ -0,0 +1,57 @@
+package com.ll.kotudy.member.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.kotudy.member.dto.reqeust.MemberJoinRequest;
+import com.ll.kotudy.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@MockBean(JpaMetamodelMappingContext.class)
+class MemerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    MemberService memberService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+        String username = "honggildong";
+        String password = "1234";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new MemberJoinRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 (username 중복)")
+    void joinFail_username() throws Exception {
+        String username = "honggildong";
+        String password = "1234";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new MemberJoinRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+}
