50ecb993a48a18afc3adb5600f220f0864bfbe91
siwookim97 <tldn12gh@gmail.com>
2023-05-16 02:49:25 +0900

    test: 로그인 테스트 추가

--- src/test/java/com/ll/kotudy/member/controller/MemerControllerTest.java
+++ src/test/java/com/ll/kotudy/member/controller/MemerControllerTest.java
@@ -2,6 +2,9 @@ package com.ll.kotudy.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.kotudy.member.dto.reqeust.MemberJoinRequest;
+import com.ll.kotudy.member.dto.reqeust.MemberLoginRequest;
+import com.ll.kotudy.member.exception.AppException;
+import com.ll.kotudy.member.exception.ErrorCode;
 import com.ll.kotudy.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,10 +13,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,19 +38,22 @@ class MemerControllerTest {
 
     @Test
     @DisplayName("회원가입 성공")
+    @WithMockUser
     void join() throws Exception {
         String username = "honggildong";
         String password = "1234";
 
         mockMvc.perform(post("/api/v1/users/join")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(new MemberJoinRequest(username, password))))
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new MemberJoinRequest(username, password))))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("회원가입 실패 (username 중복)")
+    @DisplayName("회원가입 실패 - USERNAME 중복")
+    @WithMockUser
     void joinFail_username() throws Exception {
         String username = "honggildong";
         String password = "1234";
@@ -54,9 +62,64 @@ class MemerControllerTest {
                 .thenThrow(new RuntimeException("해당 userId가 중복됩니다."));
 
         mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(new MemberJoinRequest(username, password))))
                 .andDo(print())
                 .andExpect(status().isConflict());
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    @WithMockUser
+    void login_success() throws Exception {
+        String username = "honggildong";
+        String password = "1234";
+
+        when(memberService.login(any(), any()))
+                .thenReturn("token");
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new MemberLoginRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - USERNAME 없음")
+    @WithMockUser
+    void login_fail_username() throws Exception {
+        String username = "honggildong";
+        String password = "1234";
+
+        when(memberService.login(any(), any()))
+                .thenThrow(new AppException(ErrorCode.USERNAME_NOT_FOUND, ""));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new MemberLoginRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - PASSWORD 불일치")
+    @WithMockUser
+    void login_fail_password() throws Exception {
+        String username = "honggildong";
+        String password = "1234";
+
+        when(memberService.login(any(), any()))
+                .thenThrow(new AppException(ErrorCode.INVALID_PASSWORD, ""));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new MemberLoginRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 }
