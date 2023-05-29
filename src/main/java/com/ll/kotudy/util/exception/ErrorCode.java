9220070ccd571d9caa7fb1d4c6afacdf408bd66f
siwookim97 <tldn12gh@gmail.com>
2023-05-29 23:58:45 +0900

    feat: ErrorCode 추가, ErrorResponse 변경

--- src/main/java/com/ll/kotudy/util/exception/ErrorCode.java
+++ src/main/java/com/ll/kotudy/util/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USERNAME_DUPLICATED(409, HttpStatus.CONFLICT),
     BODY_BAD_REQUEST(400, HttpStatus.BAD_REQUEST),
+    REQUEST_PARAM_BAD_REQUEST(400, HttpStatus.BAD_REQUEST),
     INVALID_PASSWORD(401, HttpStatus.UNAUTHORIZED),
     RUNTIME_EXCEPTION(500, HttpStatus.INTERNAL_SERVER_ERROR);
 
--- src/main/java/com/ll/kotudy/util/exception/ErrorResponse.java
+++ src/main/java/com/ll/kotudy/util/exception/ErrorResponse.java
@@ -4,19 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import java.sql.Timestamp;
+
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
 
+    private Timestamp timeStamp;
     private HttpStatus httpStatus;
     private int errorCode;
     private String msg;
 
     public static ErrorResponse occurred(final AppException e) {
-        return new ErrorResponse(e.getErrorCode().getHttpStatus(), e.getErrorCode().getCode(), e.getMsg());
+        return new ErrorResponse(new Timestamp(System.currentTimeMillis()),
+                e.getErrorCode().getHttpStatus(),
+                e.getErrorCode().getCode(),
+                e.getMsg());
     }
 
     public static ErrorResponse occurred(HttpStatus httpStatus, int errorCode, String msg) {
-        return new ErrorResponse(httpStatus, errorCode, msg);
+        return new ErrorResponse(new Timestamp(System.currentTimeMillis()),
+                httpStatus,
+                errorCode,
+                msg);
     }
 }
