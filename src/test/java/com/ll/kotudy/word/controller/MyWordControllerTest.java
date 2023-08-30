a34e1aa2bf12d75bc94491c62c7d817cec76b6e9
siwookim97 <tldn12gh@gmail.com>
2023-08-31 02:00:11 +0900

    test: Spring REST Docs를 위한 MyWord.DELETE Test 코드 추가

--- src/test/java/com/ll/kotudy/word/controller/MyWordControllerTest.java
+++ src/test/java/com/ll/kotudy/word/controller/MyWordControllerTest.java
@@ -2,10 +2,13 @@ package com.ll.kotudy.word.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import com.ll.kotudy.config.auth.JwtProvider;
 import com.ll.kotudy.member.domain.MemberRepository;
 import com.ll.kotudy.member.dto.reqeust.MemberJoinRequest;
 import com.ll.kotudy.member.dto.reqeust.MemberLoginRequest;
+import com.ll.kotudy.member.service.MemberService;
 import com.ll.kotudy.word.dto.request.MyWordAddRequest;
+import com.ll.kotudy.word.service.MyWordService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,8 +20,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.request.RequestDocumentation;
+import org.springframework.restdocs.snippet.Snippet;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,7 +37,9 @@ import javax.servlet.ServletException;
 import static org.springframework.restdocs.headers.HeaderDocumentation.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -48,6 +56,12 @@ class MyWordControllerTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private MyWordService myWordService;
+
+    @Autowired
+    private JwtProvider jwtProvider;
 
     final private ObjectMapper objectMapper = new ObjectMapper();
 
@@ -68,6 +82,7 @@ class MyWordControllerTest {
     public void mywWord_add() throws Exception {
         MemberJoinRequest memberJoinRequest = new MemberJoinRequest("홍길동", "qwer1234");
         MemberLoginRequest memberLoginRequest = new MemberLoginRequest("홍길동", "qwer1234");
+        MyWordAddRequest myWordAddRequest = new MyWordAddRequest("이름", "품사", "의미");
 
         mockMvc.perform(post("/api/v1/member/join")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -83,15 +98,12 @@ class MyWordControllerTest {
 
         token = JsonPath.parse(mvcResult.getResponse().getContentAsString()).read("$.accessToken");
 
-        MyWordAddRequest myWordAddRequest = new MyWordAddRequest("이름", "품사", "의미");
-        String response = "표준 한국어 대사전 Open API를 통해 단어 사랑의 검색결과는 다음과 같습니다.";
-
         mockMvc.perform(post("/api/v1/myWord")
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(myWordAddRequest))
                 )
-                      .andExpect(status().isOk())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.msg").value(("이름 단어 추가가 성공하였습니다.")))
                 .andDo(document("MyWord-add",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
@@ -111,7 +123,57 @@ class MyWordControllerTest {
                                 fieldWithPath("data.morpheme").description("저장을 요청한 단어의 품사").type(JsonFieldType.STRING),
                                 fieldWithPath("data.mean").description("저장을 요청한 단어의 의미").type(JsonFieldType.STRING)
 
-                                )))
+                        )))
+                .andDo(print());
+    }
+
+    @Test
+    @WithMockUser(username = "홍길동", roles = {"USER"})
+    public void mywWord_delete() throws Exception {
+        MemberJoinRequest memberJoinRequest = new MemberJoinRequest("홍길동", "qwer1234");
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest("홍길동", "qwer1234");
+        MyWordAddRequest myWordAddRequest = new MyWordAddRequest("이름", "품사", "의미");
+
+        mockMvc.perform(post("/api/v1/member/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberJoinRequest))
+                )
+                .andDo(print());
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/v1/member/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(memberLoginRequest))
+                )
+                .andReturn();
+
+        token = JsonPath.parse(mvcResult.getResponse().getContentAsString()).read("$.accessToken");
+        mockMvc.perform(post("/api/v1/myWord")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(myWordAddRequest))
+        )
+                        .andDo(print());
+
+        mockMvc.perform(delete("/api/v1/myWord/{myWordId}", 1)
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.msg").value(("1번호의 단어 삭제를 성공하였습니다.")))
+                .andDo(document("MyWord-delete",
+                        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        pathParameters(
+                                parameterWithName("myWordId").description("삭제 요청할 단어 ID")
+                        ),
+                        requestHeaders(headerWithName(HttpHeaders.AUTHORIZATION).description("JWT Access 토큰")),
+                        responseFields(
+                                fieldWithPath("msg").description("응답 메시지").type(JsonFieldType.STRING),
+                                fieldWithPath("data").description("삭제를 요청한 단어의 정보").type(JsonFieldType.OBJECT),
+                                fieldWithPath("data.wordId").description("삭제를 요청한 단어의 ID").type(JsonFieldType.NUMBER),
+                                fieldWithPath("data.name").description("삭제를 요청한 단어").type(JsonFieldType.STRING),
+                                fieldWithPath("data.morpheme").description("삭제를 요청한 단어의 품사").type(JsonFieldType.STRING),
+                                fieldWithPath("data.mean").description("삭제를 요청한 단어의 의미").type(JsonFieldType.STRING)
+                        )))
                 .andDo(print());
     }
 }
