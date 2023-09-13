b9d4289a67f1d6a3890982f6ca9a450203ecf519
siwookim97 <tldn12gh@gmail.com>
2023-09-14 03:01:18 +0900

    feat: Ehcache 3 사용

---
+++ src/main/java/com/ll/kotudy/logging/CacheEventLogger.java
@@ -0,0 +1,19 @@
+package com.ll.kotudy.logging;
+
+import org.ehcache.event.CacheEvent;
+import org.ehcache.event.CacheEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CacheEventLogger implements CacheEventListener<Object, Object> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CacheEventLogger.class);
+
+    @Override
+    public void onEvent(CacheEvent cacheEvent) {
+        LOG.info("Cache event = {}, Key = {},  Old value = {}, New value = {}", cacheEvent.getType(),
+                cacheEvent.getKey(), cacheEvent.getOldValue(), cacheEvent.getNewValue());
+    }
+}
--- src/main/java/com/ll/kotudy/word/service/DictionaryOpenApiService.java
+++ src/main/java/com/ll/kotudy/word/service/DictionaryOpenApiService.java
@@ -1,14 +1,13 @@
 package com.ll.kotudy.word.service;
 
-import com.ll.kotudy.word.cache.OpenApiCache;
-import com.ll.kotudy.word.domain.OpenApiCacheRepository;
 import com.ll.kotudy.word.dto.SearchedWordDto;
 import com.ll.kotudy.word.dto.WordSenceDto;
 import com.ll.kotudy.word.dto.response.SearchedWordsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -42,9 +41,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DictionaryOpenApiService {
 
-    private final OpenApiCacheRepository openApiCacheRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
-
     @Value("${korean.dictionary.key}")
     private String key;
 
@@ -54,12 +50,9 @@ public class DictionaryOpenApiService {
     @Value("${korean.dictionary.no-result}")
     private String noResult;
 
+    @Cacheable(cacheNames = "searchedWordsCache", key = "#q")
     public SearchedWordsResponse searchWords(String q)
             throws IOException, SAXException, ParserConfigurationException, XPathExpressionException, NoSuchAlgorithmException, KeyManagementException {
-
-        if (isWordCached(q)) {
-            return getSearchedWordResponseFromCache(q);
-        }
 
         String replacedQ = replaceBlank(q);
         StringBuilder requestUrl = new StringBuilder();
@@ -67,7 +60,6 @@ public class DictionaryOpenApiService {
         requestUrl.append("key=");
         requestUrl.append(key);
         requestUrl.append("&q=");
-        // TODO: replacedQ 없으면 예외 처리 RequestDTO 에서 처리 NOT EMPTY로
 
         sslTrustAllCerts();
         String resultWord = trimXmlString(requestUrl.toString(), replacedQ);
@@ -82,16 +74,6 @@ public class DictionaryOpenApiService {
         NodeList nodeList = getNodeList(resultWord);
 
         return parseXmlToResponse(replacedQ, nodeList);
-    }
-
-    private boolean isWordCached(String q) {
-        return openApiCacheRepository.findById(q).isPresent();
-    }
-
-    private SearchedWordsResponse getSearchedWordResponseFromCache(String q) {
-        List<SearchedWordDto> datum = openApiCacheRepository.findById(q).get().getDatum();
-
-        return new SearchedWordsResponse("표준 한국어 대사전 Open API를 통해 단어 " + q + "의 검색결과는 다음과 같습니다.", datum);
     }
 
     private String replaceBlank(String q) {
@@ -144,7 +126,6 @@ public class DictionaryOpenApiService {
 
         searchedWordsResponse.setMsg("표준 한국어 대사전 Open API를 통해 단어 " + replacedQ + "의 검색결과는 다음과 같습니다.");
         searchedWordsResponse.setData(searchedWordDtoList);
-        openApiCacheRepository.save(new OpenApiCache(replacedQ, searchedWordDtoList));
         return searchedWordsResponse;
     }
 
