712d700d43e08b14a727e38454a6710f0d451f7a
siwookim97 <tldn12gh@gmail.com>
2023-09-14 03:00:37 +0900

    feat: Ehcache 사용위해 설정 파일과 라이브러리 추가 및 Redis 캐싱 deprecated

--- src/main/java/com/ll/kotudy/word/domain/OpenApiCacheRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.kotudy.word.domain;
-
-import com.ll.kotudy.word.cache.OpenApiCache;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OpenApiCacheRepository extends CrudRepository<OpenApiCache, String> {
-}
