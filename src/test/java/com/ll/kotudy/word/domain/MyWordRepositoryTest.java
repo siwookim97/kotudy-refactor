d577245cee05c6565c203f5bdd653bb18199d66b
siwookim97 <tldn12gh@gmail.com>
2023-09-24 21:16:21 +0900

    test: MyWord 나만의 단어장 테스트 추가

--- src/test/java/com/ll/kotudy/word/domain/MyWordRepositoryTest.java
+++ src/test/java/com/ll/kotudy/word/domain/MyWordRepositoryTest.java
@@ -1,12 +1,23 @@
 package com.ll.kotudy.word.domain;
 
 import com.ll.kotudy.config.QuerydslConfig;
+import com.ll.kotudy.member.domain.Member;
+import com.ll.kotudy.member.domain.MemberRepository;
+import com.ll.kotudy.word.dto.QuizWordDto;
+import com.ll.kotudy.word.dto.TodayWordDto;
+import com.ll.kotudy.word.dto.request.MyWordSearchRequest;
+import com.ll.kotudy.word.dto.response.MyWordResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -14,13 +25,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @Import(QuerydslConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MyWordRepositoryTest {
 
     @Autowired
     private MyWordRepository myWordRepository;
 
+    @Autowired
+    private MemberMyWordRepository memberMyWordRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
-    @DisplayName("나만의 단어장에 단어 저장 테스트")
+    @DisplayName("단어장에 단어 저장 테스트")
     void saveMyWord() {
         // given
         MyWord createdMyWord = new MyWord("단어", "품사", "의미");
@@ -38,7 +56,7 @@ class MyWordRepositoryTest {
     }
 
     @Test
-    @DisplayName("나만의 단어장에 단어 찾기 테스트 (성공)")
+    @DisplayName("단어장에 단어 찾기 테스트 (성공)")
     void findByNameAndMorphemeAndMeanMyWord() {
         // given
         MyWord createdMyWord = new MyWord("단어", "품사", "의미");
@@ -58,7 +76,7 @@ class MyWordRepositoryTest {
     }
 
     @Test
-    @DisplayName("나만의 단어장에 단어 찾기 테스트 (실패-해당 자료 없음)")
+    @DisplayName("단어장에 단어 찾기 테스트 (실패-해당 자료 없음)")
     void findByNameAndMorphemeAndMeanMyWord_fail() {
         // given
         MyWord createdMyWord = new MyWord("단어", "품사", "의미");
@@ -70,4 +88,151 @@ class MyWordRepositoryTest {
         // then
         assertThat(foundOptionalMyWord.isPresent()).isFalse();
     }
+
+    @Test
+    @DisplayName("단어장에서 랜덤 40개 반환 테스트")
+    void findMyWordDistinctRandomForQuizTest() {
+        // given
+        for (int i = 0; i < 50; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            myWordRepository.save(createdMyWord);
+        }
+
+        // when
+        List<QuizWordDto> foundQuizWordDtoList = myWordRepository.findMyWordDistinctRandomForQuiz();
+
+        // then
+        assertThat(foundQuizWordDtoList.size()).isEqualTo(40);
+    }
+
+    @Test
+    @DisplayName("단어장에서 랜덤 40개 반환 중복값 유무 테스트")
+    void findMyWordDistinctRandomForQuizDistinctTest() {
+        // given
+        for (int i = 0; i < 50; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            myWordRepository.save(createdMyWord);
+        }
+
+        // when
+        List<QuizWordDto> foundQuizWordDtoList = myWordRepository.findMyWordDistinctRandomForQuiz();
+
+        // then
+        assertThat(foundQuizWordDtoList.stream().distinct().count()).isEqualTo(40);
+    }
+
+    @Test
+    @DisplayName("단어장에서 랜텀 10개 반환 테스트")
+    void findRandomDistinctMyWordsTest() {
+        // given
+        for (int i = 0; i < 20; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            myWordRepository.save(createdMyWord);
+        }
+
+        // when
+        List<TodayWordDto> foundTodayWordDtoList = myWordRepository.findRandomDistinctMyWords();
+
+        // then
+        assertThat(foundTodayWordDtoList.size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("단어장에서 랜텀 10개 반환 테스트")
+    void findRandomDistinctMyWordsDistinctTest() {
+        // given
+        for (int i = 0; i < 20; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            myWordRepository.save(createdMyWord);
+        }
+
+        // when
+        List<TodayWordDto> foundTodayWordDtoList = myWordRepository.findRandomDistinctMyWords();
+
+        // then
+        assertThat(foundTodayWordDtoList.stream().distinct().count()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("단어장 페이징 쿼리 테스트 - (성공-단어, 품사 모두 검색)")
+    void findMyWordByConditionMyWordTest() {
+        // given
+        MyWordSearchRequest request = new MyWordSearchRequest("2", "2"); // 2가 들어간 단어와 품사 검색
+        Pageable pageRequest = PageRequest.of(0, 10);
+        Member createdMember = new Member("홍길동", "password");
+        memberRepository.save(createdMember);
+
+        for (int i = 0; i < 20; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            MemberMyWord createdMemberMyWord = new MemberMyWord(createdMember, createdMyWord);
+
+            createdMyWord.addMemberMyWord(createdMemberMyWord);
+            myWordRepository.save(createdMyWord);
+            memberMyWordRepository.save(createdMemberMyWord);
+        }
+
+        // when
+        Page<MyWordResponse> foundMyWordResponsePage = myWordRepository
+                .findMyWordByConditionMyWord(request, pageRequest, createdMember.getId());
+
+        // then
+        assertThat(foundMyWordResponsePage.getTotalElements()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("단어장 페이징 쿼리 테스트 - (성공-단어만 검색)")
+    void findMyWordByConditionMyWordNameSearchTest() {
+        // given
+        MyWordSearchRequest request = MyWordSearchRequest.builder()
+                .name("2")
+                .build(); // 2가 들어간 단어와 품사 검색
+        Pageable pageRequest = PageRequest.of(0, 10);
+        Member createdMember = new Member("홍길동", "password");
+        memberRepository.save(createdMember);
+
+        for (int i = 0; i < 20; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            MemberMyWord createdMemberMyWord = new MemberMyWord(createdMember, createdMyWord);
+
+            createdMyWord.addMemberMyWord(createdMemberMyWord);
+            myWordRepository.save(createdMyWord);
+            memberMyWordRepository.save(createdMemberMyWord);
+        }
+
+        // when
+        Page<MyWordResponse> foundMyWordResponsePage = myWordRepository
+                .findMyWordByConditionMyWord(request, pageRequest, createdMember.getId());
+
+        // then
+        assertThat(foundMyWordResponsePage.getTotalElements()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("단어장 페이징 쿼리 테스트 - (성공-품사만 검색)")
+    void findMyWordByConditionMyWordMorphemeSearchTest() {
+        // given
+        MyWordSearchRequest request = MyWordSearchRequest.builder()
+                .morpheme("2")
+                .build(); // 2가 들어간 단어와 품사 검색
+        Pageable pageRequest = PageRequest.of(0, 10);
+        Member createdMember = new Member("홍길동", "password");
+        memberRepository.save(createdMember);
+
+        for (int i = 0; i < 20; i++) {
+            MyWord createdMyWord = new MyWord("단어" + i, "품사" + i, "의미" + i);
+            MemberMyWord createdMemberMyWord = new MemberMyWord(createdMember, createdMyWord);
+
+            createdMyWord.addMemberMyWord(createdMemberMyWord);
+            myWordRepository.save(createdMyWord);
+            memberMyWordRepository.save(createdMemberMyWord);
+        }
+
+        // when
+        Page<MyWordResponse> foundMyWordResponsePage = myWordRepository
+                .findMyWordByConditionMyWord(request, pageRequest, createdMember.getId());
+
+        // then
+        assertThat(foundMyWordResponsePage.getTotalElements()).isEqualTo(2);
+    }
 }
+
